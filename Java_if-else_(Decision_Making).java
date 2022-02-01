730c1e38cd6403e0b978ed507eb0488ed6de5ded
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-01 18:04:10 +0530

    Add files via upload

---
+++ Java_if-else_(Decision_Making).java
@@ -0,0 +1,16 @@
+class Solution{
+    static String compareNM(int n,int m){
+        if(n<m)
+        {
+            return "lesser";
+        }
+        else if(n>m)
+        {
+            return "greater";
+        }
+        else
+        {
+            return "equal";
+        }
+    }
+}
