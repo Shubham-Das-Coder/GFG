aac44b8ce14e370121c6f3feebb842811eb3063c
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-01 15:59:14 +0530

    Add files via upload

---
+++ 1's_Complement.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    string onesComplement(string S, int N)
+    {
+        string ans = "";
+        for (int i = 0; i < N; i++)
+        {
+            if (S[i] == '0')
+                ans += '1';
+            else
+                ans += '0';
+        }
+        return ans;
+    }
+};
