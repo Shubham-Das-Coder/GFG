3d1e2285ed56ea6e0c904677810fee4dd582c5b9
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-01 18:38:56 +0530

    Add files via upload

---
+++ Remove_Spaces.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    string modify(string s)
+    {
+        string str = "";
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] != ' ')
+            {
+                str += s[i];
+            }
+        }
+        return str;
+    }
+};
